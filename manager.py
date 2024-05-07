b9c3d25bc91c12bf857fbdf5aa30954004ee7504
MohammadMahdi-Sharifbeigy <154359434+MohammadMahdi-Sharifbeigy@users.noreply.github.com>
2024-05-07 15:48:39 +0330

    fix creatproject and addmember

--- manager.py
+++ manager.py
@@ -33,6 +33,10 @@ project_parser.add_argument('--start_date', help='Project Start Date (dd/mm/yyyy
 
 project_parser = subparsers.add_parser('purge-data')
 
+member_parser = subparsers.add_parser('add-member')
+member_parser.add_argument('--project_id', help='Project ID', required=True)
+member_parser.add_argument('--username', help='Username of the member to add', required=True)
+
 def create_admin(username, password):
     try:
         with open("data.json", "r") as f:
@@ -113,11 +117,53 @@ def get_project(project_id):
     except FileNotFoundError:
         return None
     
+    if 'projects' not in data:
+        return None
+    
     for project in data["projects"]:
         if project["id"] == project_id:
             return project
     return None
 
+
+def get_user(username):
+    try:
+        with open(DATA_FILE, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        return None
+    
+    for user in data["users"]:
+        if user["username"] == username:
+            return user
+    return None
+
+def add_member(project_id, username):
+    try:
+        with open(DATA_FILE, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print("[bold red]Error: Data file not found![/]")
+        return None
+
+    project = get_project(project_id)
+    if not project:
+        print(f"[bold red]Error: Project '{project_id}' not found![/]")
+        return None
+    user = get_user(username)
+    if not user:
+        print(f"[bold red]Error: User '{username}' not found![/]")
+        return None
+    if username in project["members"]:
+        print(f"[bold red]Error: User '{username}' is already a member of the project![/]")
+        return None
+
+    project["members"].append(username)
+
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=2)
+
+    print(f"[green]User '{username}' successfully added to the project![/]")   
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -130,5 +176,7 @@ if __name__ == "__main__":
         purge_data(input("[bold red]Are you sure you want to erase all data? (y/n): "))
     elif args.command == 'create-project':
         create_project(args.project_id, args.title, args.start_date)
+    elif args.command == 'add-member':
+        add_member(args.project_id, args.username)
     else:
         print("[red]Invalid command![/]")
